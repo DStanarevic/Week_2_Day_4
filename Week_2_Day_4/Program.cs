ea6b8771a7f112b1b75ca23fe3b1fa7d2ffe8b52
DStanarevic <DejanStanarevic@gmail.com>
2017-10-05 15:01:01 -0400

    Initial commit

---
+++ Week_2_Day_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objet Oriented Programming, thinking of our program as a real world project. 
+            //Objects have states and behaviors.
+
+        }
+    }
+}
