895dccb920b575340237e082b24f5683ce8fe0c6
DStanarevic <DejanStanarevic@gmail.com>
2017-10-05 15:14:27 -0400

    update

--- Week_2_Day_4/Cat.cs
+++ Week_2_Day_4/Cat.cs
@@ -8,5 +8,28 @@ namespace Week_2_Day_4
 {
     class Cat
     {
+        //States - The private modifier means that it stays inside this class.
+        //Different types of states.
+        //Fields
+       
+        private string name;
+        //Properties are part of the program that let us interact with the characteristics of the fields.
+        //Properties are the mama bear/bouncer for the fields.
+        public string Name //Property is upper case name with get set. 
+        {
+            get { return this.name; } // 
+            set { this.name = value; }
+
+        }
+
+
+
+
+        //Behaviors
+        //Constructors - The constructor will be cat, it's treated like a method. Each class needs a constructor so that when we are in another program we can create an object.
+        public Cat()
+        {
+            //default constructor, it takes no parameters. 
+        }
     }
 }
